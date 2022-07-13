3fe1f35e6ddb887e45f0403f421560ea26178611
pchomiak <pchomiak@gmail.com>
2022-07-13 18:17:53 +0200

    cssy do paginacji

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import { fetchEvents } from "./js/fetchEvents";
 import { countryList } from "./js/countryList";
 import { searchEvents} from "./js/searchEvent";
 import { renderEvents } from "./js/searchEvent";
-
+import { pagination } from "./js/pagination";
 

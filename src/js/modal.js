83c1387e29fd7efaa37ced947d77658a727e2e44
Paweł Rosłaniec <pawel94roslaniec@gmail.com>
2022-07-14 15:55:35 +0200

    feature/modal (just beginning

---
+++ src/js/modal.js
@@ -0,0 +1,29 @@
+events.addEventListener('click', renderModal);
+
+function renderModal(e) {
+    events.classList.add(hide);
+    const markup =
+    `<div class="modal">
+        <div class="modal__header">
+            <img src="${e.target.src}">
+        </div>
+        <div class="modal__main">
+            <img class="event-img">
+            <div class="modal__about">
+                <span class="modal__about-span">INFO</span>
+                ${e.targer}
+                <span class="modal__about-span">WHEN</span>
+                ${e.target}
+                <span class="modal__about-span">WHERE</span>
+                ${e.target}
+                <span class="modal__about-span">WHO</span>
+                ${e.target}
+                <span class="modal__about-span">PRICES</span>
+                ${e.target}
+            </div>
+        </div>
+        <button class="modal__more"> More from this author </button>
+    <div>
+    </div>`
+}
+

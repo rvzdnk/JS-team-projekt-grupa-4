73661324a315442bf79df9d4d7dc53b8e70805f7
Patrycja Wiza <95050810+PatrycjaWiza@users.noreply.github.com>
2022-07-18 16:57:27 +0200

    Update searchEvent.js

--- src/js/searchEvent.js
+++ src/js/searchEvent.js
@@ -1,7 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
-import { pagination, paginationAfterSearch} from './pagination';
-import { pageNr} from './pagination';
+import { pagination, paginationAfterSearch, pageNr} from './pagination';
 import { fetchEvents } from './fetchEvents';
 
 const searchForm = document.querySelector('.form');

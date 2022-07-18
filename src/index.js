7c7fdb0a0957c69d043ca5cc89db75a6765b6a6d
rvzdnk <rafal93zdunek@gmail.com>
2022-07-18 23:01:49 +0200

    preloader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+const preloader = document.querySelector('#preloader');
+
+preloader.classList.add('show-preloader');
+
+window.addEventListener('load', function () {
+  preloader.classList.remove('show-preloader');
+});
+
+
 import { chooseCountries } from "./js/chooseCountry";
 import { fetchEvents } from "./js/fetchEvents";
 import { countryList } from "./js/countryList";

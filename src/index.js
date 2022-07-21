a32dfeca7532a2cea3f3bae96665e947a7f25eec
PatrycjaWiza <95050810+PatrycjaWiza@users.noreply.github.com>
2022-07-21 18:04:11 +0200

    Loading updates
    Preload setTimeout increased and set on body
    Loading for fetch added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 const preloader = document.querySelector('#preloader');
-
+const body = document.querySelector('body');
 preloader.classList.add('show-preloader');
 
-window.addEventListener('load', function () {
+body.addEventListener('load', setTimeout(preload, 900));
+
+function preload() {
   preloader.classList.remove('show-preloader');
-});
-
-
-import { chooseCountries } from "./js/chooseCountry";
-import { fetchEvents } from "./js/fetchEvents";
-import { countryList } from "./js/countryList";
-import { searchEvents} from "./js/searchEvent";
-import { renderEvents } from "./js/searchEvent";
-import { pagination } from "./js/pagination";
-import { modal } from "./js/modal";
-
+}
+import { chooseCountries } from './js/chooseCountry';
+import { fetchEvents } from './js/fetchEvents';
+import { countryList } from './js/countryList';
+import { searchEvents } from './js/searchEvent';
+import { renderEvents } from './js/searchEvent';
+import { pagination } from './js/pagination';
+import { modal } from './js/modal';
